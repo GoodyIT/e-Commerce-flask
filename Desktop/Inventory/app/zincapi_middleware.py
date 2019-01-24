fcd136fd5c63cd73d6f4e0fc7bffb91b4e7af919
radostnia <rado.sto@gmail.com>
2019-01-24 17:30:06 +0200

    Get request id in the middleware.

--- Desktop/Inventory/app/zincapi_middleware.py
+++ Desktop/Inventory/app/zincapi_middleware.py
@@ -1,5 +1,9 @@
+import json
+
+from pymongo import MongoClient
 from werkzeug.wrappers import Request
 
+#from app import db
 from .zincapi_communication import post_shipping_request, post_cancellation_request
 
 class ZincapiMiddleware(object):
@@ -13,19 +17,30 @@ class ZincapiMiddleware(object):
         print('Function called, HTTP_HOST = ', environ['HTTP_HOST'])
         print('--------------------------')
         import pprint
-
         if '/orders' == environ['PATH_INFO'] and 'POST' == environ['REQUEST_METHOD']:
-
-            print('type(environ) ------------------- >', type(environ))
-
+            #print('type(environ) ------------------- >', type(environ))
             request = Request(environ)
 
-            pprint.pprint(self._flask_app.request.json)
-            pass
+            client = MongoClient('mongodb://localhost:27017/')
+            db = client.test_inv
 
-        #item = request.json
-        #print('-------- item --------- ', item)
+            order_id = json.loads(request.data.decode("utf-8"))['id']
+            new_order = db.Queue.find_one({'order_id':order_id})
+            request_id = post_shipping_request(new_order['item_id'], new_order['quantity'])
 
+            new_order['shipped'] = 'awaiting'
+            if request_id:
+                new_order['request_id'] = request_id
+
+            db.Queue.remove({'order_id':item['id']})
+            db.Orders.insert_one(new_order)
+
+            #print('========================================')
+            #print('request_id ==> ', '\n', request_id)
+            #print('========================================')
+
+        # elif is shpping on load (listen ajax call)
+             # get all orders and call shipping status
 
         return self.app(environ, start_response)
 
